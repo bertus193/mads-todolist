fc8a8d7c32aa06de19381811eabe30ba949eb925
Alberto <albrigar93@gmail.com>
2016-10-20 12:18:36 +0200

    Arreglo de Tarea

--- app/models/Tarea.java
+++ app/models/Tarea.java
@@ -5,38 +5,27 @@ import javax.persistence.*;
 import play.data.validation.Constraints;
 import play.data.format.*;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
 @Entity
-@Table(uniqueConstraints={@UniqueConstraint(columnNames = {"login"})})
 public class Tarea {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     public Integer id;
     @Constraints.Required
-    public String login;
-    public String nombre;
-    public String apellidos;
-    public String eMail;
     public String descripcion;
-
     @ManyToOne
     @JoinColumn(name="usuarioId")
     public Usuario usuario;
 
+    // Un constructor vacío necesario para JPA
     public Tarea() {}
 
+    // El constructor principal con los campos obligatorios
     public Tarea(String descripcion) {
-      this.descripcion = descripcion;
+        this.descripcion = descripcion;
     }
 
     public String toString() {
         return String.format("Tarea id: %s descripcion: %s", id, descripcion);
-    }
-
-    public void nulificaAtributos() {
-          if (descripcion != null && descripcion.isEmpty()) descripcion = null;
     }
 
     public Tarea copy() {
