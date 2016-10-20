34238fe252f047108c90593db954575ae295f79b
Alberto <albrigar93@gmail.com>
2016-10-20 14:32:17 +0200

    Commit inicial Add Tarea Usuario

--- app/models/TareaDAO.java
+++ app/models/TareaDAO.java
@@ -8,6 +8,22 @@ import java.util.List;
 import java.util.Date;
 
 public class TareaDAO {
+    public static Tarea create (Tarea tarea) {
+      try{
+        tarea.nulificaAtributos();
+        JPA.em().persist(tarea);
+        // Hacemos un flush y un refresh para asegurarnos de que se realiza
+        // la creación en la BD y se devuelve el id inicializado
+        JPA.em().flush();
+        JPA.em().refresh(tarea);
+        Logger.debug(tarea.toString());
+        return tarea;
+      }
+      catch(PersistenceException ex){
+        return null;
+      }
+    }
+
     public static Tarea find(Integer idTarea) {
         return JPA.em().find(Tarea.class, idTarea);
     }
